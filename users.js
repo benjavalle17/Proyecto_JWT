d21f2d623185b1a9189e4e9fa2f2dcf134e09bb1
Seba14k <122108400+Seba14k@users.noreply.github.com>
2026-04-14 14:46:36 -0400

    Delete users.js

--- users.js
+++
@@ -1,9 +0,0 @@
-// Arreglo de usuarios ficticios para probar el inicio de sesión
-const users = [
-  { id: 1, username: "sebastian", password: "seba1234" },
-  { id: 2, username: "benjamin", password: "benja1234" },
-  { id: 3, username: "vicente", password: "vicho1234" }
-];
-
-// Exporta el arreglo para poder usarlo en server.js
-module.exports = users;
