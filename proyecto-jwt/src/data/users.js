30bead048631e50895d113454acfaaacea2eafff
valfo13 <73298563+V-bit512@users.noreply.github.com>
2026-04-14 20:00:19 -0400

    Update users.js

--- proyecto-jwt/src/data/users.js
+++ proyecto-jwt/src/data/users.js
@@ -4,6 +4,5 @@ const users = [
   { id: 2, username: "benjamin", password: "benja1234" },
   { id: 3, username: "vicente", password: "vicho1234" }
 ];
-
 // Exporta el arreglo para poder usarlo en server.js
 module.exports = users;
